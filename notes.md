df78921747341f0bfdbab7a2746ca46c5f2f52cc
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-07-08 22:44:41 -0300

    feat: learn about parameters types

--- notes.md
+++ notes.md
@@ -2,8 +2,32 @@
 
 ## Métodos HTTP
 
-- GET: Buscar uma informação dentro do servidor
-- POST: Inserir uma informação no servidor
-- PUT: Alterar uma informação no servidor
-- PATCH: Alterar uma informação específica no servidor
-- DELETE: Deletar uma informação no servidor
+- GET: Buscar uma informação dentro do servidor.
+- POST: Inserir uma informação no servidor.
+- PUT: Alterar uma informação no servidor.
+- PATCH: Alterar uma informação específica no servidor.
+- DELETE: Deletar uma informação no servidor.
+
+## Tipos de parâmetros
+
+- Route Params: usar para identificar um recurso para editar, deletar ou buscar.
+
+  - É obrigatório.
+
+  Exemplo: `/endpoint/:param`
+
+- Query Params: usar para paginação, filtro.
+
+  - Pode ser opcional.
+
+  Exemplo: `/posts?page=4&orderBy=name`
+
+- Body Pams: usar para inserção ou alteração de recursos.
+
+  Exemplo:
+
+  ```json
+  {
+    "name": "Ignite NodeJS"
+  }
+  ```
--- src/index.js
+++ src/index.js
@@ -1,28 +1,49 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 app.get('/courses', (request, response) => {
+  const query = request.query
+
+  console.log({ params, query })
+
   const courses = ['Curso 1', 'Curso 2', 'Curso 3']
   return response.json(courses)
 })
 
 app.post('/courses', (request, response) => {
+  const body = request.body
+
+  console.log({ body })
+
   const courses = ['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']
   return response.json(courses)
 })
 
 app.put('/courses/:id', (request, response) => {
+  const params = request.params
+
+  console.log({ params })
+
   const courses = ['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']
   return response.json(courses)
 })
 
 app.patch('/courses/:id', (request, response) => {
+  const params = request.params
+
+  console.log({ params })
+
   const courses = ['Curso 7', 'Curso 2', 'Curso 3', 'Curso 4']
   return response.json(courses)
 })
 
 app.delete('/courses/:id', (request, response) => {
+  const params = request.params
+
+  console.log({ params })
+
   const courses = ['Curso 7', 'Curso 3', 'Curso 4']
   return response.json(courses)
 })

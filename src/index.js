7ed81209d08e760ff102dc50c8ed1fe4f3ef9704
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-07-08 22:09:02 -0300

    feat: adds more endpoints

--- src/index.js
+++ src/index.js
@@ -2,8 +2,29 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Ignite! 🚀' })
+app.get('/courses', (request, response) => {
+  const courses = ['Curso 1', 'Curso 2', 'Curso 3']
+  return response.json(courses)
+})
+
+app.post('/courses', (request, response) => {
+  const courses = ['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']
+  return response.json(courses)
+})
+
+app.put('/courses/:id', (request, response) => {
+  const courses = ['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']
+  return response.json(courses)
+})
+
+app.patch('/courses/:id', (request, response) => {
+  const courses = ['Curso 7', 'Curso 2', 'Curso 3', 'Curso 4']
+  return response.json(courses)
+})
+
+app.delete('/courses/:id', (request, response) => {
+  const courses = ['Curso 7', 'Curso 3', 'Curso 4']
+  return response.json(courses)
 })
 
 app.listen(3333, () => console.log('🎈 Hello!'))
